fa44b8d790af31670bfc104e1aca09dae47f6b34
Roland Start <rstart@hotmail.com>
2026-06-27 03:38:16 +0000

    File changes

--- src/components/TroutIcon.jsx
+++ src/components/TroutIcon.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const TROUT_IMG_URL =
-  "https://media.base44.com/images/public/6a3f2458eb55a5d860886e35/528918986_Trout.png";
+  "https://media.base44.com/images/public/6a3f2458eb55a5d860886e35/552bbaffa_PermitWt.png";
 
 export default function TroutIcon({ className = "w-7 h-7" }) {
   return (
