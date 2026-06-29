1daa12ab7cdb69674c7cb0cc9665c5b8955eaa76
Roland Start <rstart@hotmail.com>
2026-06-29 02:44:34 +0000

    File changes

--- src/components/LureIcon.jsx
+++ src/components/LureIcon.jsx
@@ -14,13 +14,13 @@ export default function LureIcon({ className, ...props }) {
       <circle cx="12" cy="3" r="2" />
       
       {/* straight shaft */}
-      <rect x="11" y="5" width="2" height="10" />
+      <rect x="11" y="5" width="2" height="9.5" rx="1" />
       
-      {/* J-shaped hook curve */}
-      <path d="M 11 15 Q 5 15 5 19 Q 5 22 9 22 L 13 22 Q 13 18 12 15" fill="currentColor" />
+      {/* J-hook: main curved body */}
+      <path d="M 11 14.5 C 8 14.5 5 16.5 5 19.5 C 5 22 7.5 23 10.5 23 C 13 23 13 21 13 19 L 13 14.5" fill="currentColor" />
       
-      {/* barb point */}
-      <polygon points="6,18 4,20 6,21" fill="currentColor" />
+      {/* barb point inside the hook */}
+      <polygon points="8,19 6,20.5 7.5,21" fill="currentColor" />
     </svg>
   );
 }
