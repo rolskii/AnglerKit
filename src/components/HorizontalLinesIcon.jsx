e5d3d2d1743817f75c5f9dd4b0e35ad59cb5f2d5
Roland Start <rstart@hotmail.com>
2026-06-29 14:33:58 +0000

    File changes

--- src/components/HorizontalLinesIcon.jsx
+++ src/components/HorizontalLinesIcon.jsx
@@ -6,7 +6,7 @@ export default function HorizontalLinesIcon({ className, ...props }) {
       viewBox="0 0 24 24"
       fill="none"
       stroke="currentColor"
-      strokeWidth="2"
+      strokeWidth="1.5"
       strokeLinecap="round"
       strokeLinejoin="round"
       className={className}
