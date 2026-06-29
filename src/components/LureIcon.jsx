a324af9360597ca89535de7fd17c739a8bf7a0af
Roland Start <rstart@hotmail.com>
2026-06-29 02:47:15 +0000

    File changes

--- src/components/LureIcon.jsx
+++ src/components/LureIcon.jsx
@@ -2,29 +2,11 @@ import React from "react";
 
 export default function LureIcon({ className, ...props }) {
   return (
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      viewBox="0 0 24 24"
-      fill="none"
-      stroke="currentColor"
-      strokeWidth="2"
-      strokeLinecap="round"
-      strokeLinejoin="round"
+    <img
+      src="https://media.base44.com/images/public/6a3f2458eb55a5d860886e35/24434a246_Screenshot2026-06-28223854.png"
+      alt="Lure icon"
       className={className}
-      aria-hidden="true"
       {...props}
-    >
-      {/* eye at top */}
-      <circle cx="12" cy="3" r="1.5" />
-      
-      {/* straight shaft down */}
-      <line x1="12" y1="4.5" x2="12" y2="12" />
-      
-      {/* J-hook curve - start at shaft end, curve down and around */}
-      <path d="M 12 12 Q 8 12 6 14 Q 4 16 4 18.5 Q 4 21 6.5 22" />
-      
-      {/* barb - small angled line inside the hook */}
-      <line x1="6" y1="17" x2="3.5" y2="19" strokeWidth="1.5" />
-    </svg>
+    />
   );
 }
