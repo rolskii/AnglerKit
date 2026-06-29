c71c470c002d9a034a9ab643bb14ece940023ba6
Roland Start <rstart@hotmail.com>
2026-06-29 02:48:11 +0000

    File changes

--- src/components/LureIcon.jsx
+++ src/components/LureIcon.jsx
@@ -2,11 +2,13 @@ import React from "react";
 
 export default function LureIcon({ className, ...props }) {
   return (
-    <img
-      src="https://media.base44.com/images/public/6a3f2458eb55a5d860886e35/24434a246_Screenshot2026-06-28223854.png"
-      alt="Lure icon"
-      className={className}
-      {...props}
-    />
+    <div className="flex h-11 w-11 items-center justify-center rounded-xl bg-primary/10 text-primary">
+      <img
+        src="https://media.base44.com/images/public/6a3f2458eb55a5d860886e35/24434a246_Screenshot2026-06-28223854.png"
+        alt="Lure icon"
+        className="w-6 h-6"
+        {...props}
+      />
+    </div>
   );
 }
