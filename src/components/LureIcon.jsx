423f2bbc27796a57b3ff20941992ff92dda5f0de
Roland Start <rstart@hotmail.com>
2026-06-29 03:06:05 +0000

    File changes

--- src/components/LureIcon.jsx
+++ src/components/LureIcon.jsx
@@ -4,24 +4,24 @@ export default function LureIcon({ className, ...props }) {
   return (
     <div className="flex h-11 w-11 items-center justify-center rounded-xl bg-primary/10 text-primary">
       <svg
-        viewBox="0 0 20 28"
+        viewBox="0 0 24 32"
         width="28"
         height="28"
         fill="currentColor"
         className="text-primary"
         {...props}
       >
-        {/* Round eye at top */}
-        <circle cx="10" cy="2" r="1.5" />
+        {/* Eye circle */}
+        <circle cx="12" cy="3" r="2" />
         
-        {/* Straight vertical shaft */}
-        <rect x="9.2" y="3.8" width="1.6" height="10" rx="0.8" />
+        {/* Shaft */}
+        <rect x="10.5" y="5" width="3" height="9" />
         
-        {/* Smooth hook curve - large U bend at bottom */}
-        <path d="M 9.2 13.8 Q 7.5 15.5 6 17.5 Q 4.5 19.5 4 21.5 Q 3.8 23.5 5.5 24 Q 7 24.5 8.5 22.5 Q 10 20 10.8 13.8 Z" />
+        {/* Hook curve bottom - thick U shape */}
+        <path d="M 10.5 14 Q 7 17 3.5 22 Q 2 24 4 25.5 Q 6 27 8.5 24 Q 12 19 13.5 14 Z" />
         
-        {/* Sharp barb triangle on left */}
-        <polygon points="5,17.5 2,15 4,16.5" />
+        {/* Left barb point */}
+        <polygon points="4,18 1,15.5 3.5,17" />
       </svg>
     </div>
   );
