a9ab97f5c3a3d21888cec59abe7697630a9ee693
Roland Start <rstart@hotmail.com>
2026-06-29 23:36:59 +0000

    File changes

--- src/components/FishingHookIcon.jsx
+++ src/components/FishingHookIcon.jsx
@@ -4,7 +4,7 @@ export default function FishingHookIcon({ className = "" }) {
       data="https://media.base44.com/images/public/6a3f2458eb55a5d860886e35/8eed12276_fishing-hook.svg"
       type="image/svg+xml"
       className={`w-8 h-8 ${className}`}
-      style={{ color: "hsl(170, 70%, 32%)" }}
+      style={{ filter: "brightness(0) saturate(100%) invert(95%) sepia(70%) hue-rotate(170deg) saturate(2.5)" }}
     />
   );
 }
