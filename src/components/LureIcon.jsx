c8670d996f7e23e78c8008871b00313665ae04d8
Roland Start <rstart@hotmail.com>
2026-06-29 02:38:32 +0000

    File changes

--- src/components/LureIcon.jsx
+++ src/components/LureIcon.jsx
@@ -11,16 +11,16 @@ export default function LureIcon({ className, ...props }) {
       {...props}
     >
       {/* eye - circle at top */}
-      <circle cx="12" cy="2.5" r="1.5" />
+      <circle cx="12" cy="3" r="1.5" />
       
       {/* shank - vertical bar */}
-      <rect x="11" y="4.5" width="2" height="7" rx="1" />
+      <rect x="11" y="4.5" width="2" height="6.5" rx="1" />
       
-      {/* left hook - curved downward */}
-      <path d="M 11 11.5 Q 6 11.5 5.5 15 Q 5.2 17.5 7 19 Q 8.5 20 10 18.5 Q 11 16 11 11.5" />
+      {/* left hook curve */}
+      <path d="M 11 11 Q 5.5 11 4.5 16 Q 4 18.5 6.5 20.5 Q 8 21.5 9.5 19.5 Q 10.5 17 11 11" fill="currentColor" />
       
-      {/* right hook - curved downward */}
-      <path d="M 13 11.5 Q 18 11.5 18.5 15 Q 18.8 17.5 17 19 Q 15.5 20 14 18.5 Q 13 16 13 11.5" />
+      {/* right hook curve */}
+      <path d="M 13 11 Q 18.5 11 19.5 16 Q 20 18.5 17.5 20.5 Q 16 21.5 14.5 19.5 Q 13.5 17 13 11" fill="currentColor" />
     </svg>
   );
 }
