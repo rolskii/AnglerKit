06f7d4bf1f536b7ac442c27811effca1c3068871
Roland Start <rstart@hotmail.com>
2026-06-29 02:14:18 +0000

    File changes

--- src/components/LineSpoolIcon.jsx
+++ src/components/LineSpoolIcon.jsx
@@ -14,19 +14,21 @@ export default function LineSpoolIcon({ className, ...props }) {
       aria-hidden="true"
       {...props}
     >
-      {/* side flanges */}
-      <path d="M4 5 V19" strokeWidth="2.6" />
-      <path d="M20 5 V19" strokeWidth="2.6" />
-      {/* wound line strokes */}
-      <path d="M6 6 V18" strokeWidth="1.5" />
-      <path d="M8 6 V18" strokeWidth="1.5" />
-      <path d="M10 6 V18" strokeWidth="1.5" />
-      <path d="M12 6 V18" strokeWidth="1.5" />
-      <path d="M14 6 V18" strokeWidth="1.5" />
-      <path d="M16 6 V18" strokeWidth="1.5" />
-      <path d="M18 6 V18" strokeWidth="1.5" />
-      {/* thread tail arcing off the right flange */}
-      <path d="M20 5 C15 0 10 5 5 2.5" strokeWidth="1.5" />
+      <g transform="translate(12 12) scale(1.12) translate(-12 -12)">
+        {/* side flanges */}
+        <path d="M4 4.5 V19.5" strokeWidth="3" />
+        <path d="M20 4.5 V19.5" strokeWidth="3" />
+        {/* wound line strokes */}
+        <path d="M6 6 V18" strokeWidth="2" />
+        <path d="M8 6 V18" strokeWidth="2" />
+        <path d="M10 6 V18" strokeWidth="2" />
+        <path d="M12 6 V18" strokeWidth="2" />
+        <path d="M14 6 V18" strokeWidth="2" />
+        <path d="M16 6 V18" strokeWidth="2" />
+        <path d="M18 6 V18" strokeWidth="2" />
+        {/* thread tail arcing off the right flange */}
+        <path d="M20 4.5 C15 -0.5 10 5 4.5 2" strokeWidth="2" />
+      </g>
     </svg>
   );
 }
