f79b68c83b175dce0b21042d727ede59ff6f7bf1
Roland Start <rstart@hotmail.com>
2026-06-29 02:50:10 +0000

    File changes

--- src/components/LureIcon.jsx
+++ src/components/LureIcon.jsx
@@ -6,7 +6,7 @@ export default function LureIcon({ className, ...props }) {
       <img
         src="https://media.base44.com/images/public/6a3f2458eb55a5d860886e35/24434a246_Screenshot2026-06-28223854.png"
         alt="Lure icon"
-        className="w-6 h-6"
+        className="w-7 h-7 text-primary"
         {...props}
       />
     </div>
