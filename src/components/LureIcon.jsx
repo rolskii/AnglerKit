391aeffdb2c0de4e28d101e0c6efebb52960d943
Roland Start <rstart@hotmail.com>
2026-06-29 02:55:05 +0000

    File changes

--- src/components/LureIcon.jsx
+++ src/components/LureIcon.jsx
@@ -7,7 +7,7 @@ export default function LureIcon({ className, ...props }) {
         src="https://media.base44.com/images/public/6a3f2458eb55a5d860886e35/24434a246_Screenshot2026-06-28223854.png"
         alt="Lure icon"
         className="w-7 h-7"
-        style={{ filter: 'brightness(0) saturate(100%) invert(1) sepia(0.6) hue-rotate(120deg)' }}
+        style={{ filter: 'hue-rotate(120deg) saturate(1.2)' }}
         {...props}
       />
     </div>
