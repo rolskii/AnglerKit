4987008f018828e984a15d6a19233e51fc6ca09c
Roland Start <rstart@hotmail.com>
2026-06-30 13:42:22 +0000

    File changes

--- src/components/FishIcon.jsx
+++ src/components/FishIcon.jsx
@@ -13,7 +13,7 @@ export default function FishIcon({ className = "", ...props }) {
       {...props}
     >
       <path d="M3 12c3-4 7-6 12-6 3 0 6 2 6 6s-3 6-6 6c-5 0-9-2-12-6Z" />
-      <path d="M3 12l-2-2v4l2-2Z" />
+      <path d="M1 12l2-3v6l-2-3Z" />
       <circle cx="15" cy="10.5" r="0.8" fill="currentColor" stroke="none" />
     </svg>
   );
