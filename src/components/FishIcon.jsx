2292380418804dabfc01ba4bc89b93bcda01b258
Roland Start <rstart@hotmail.com>
2026-06-30 13:45:56 +0000

    File changes

--- src/components/FishIcon.jsx
+++ src/components/FishIcon.jsx
@@ -8,6 +8,7 @@ export default function FishIcon({ className = "", ...props }) {
       alt=""
       aria-hidden="true"
       className={`${className} object-contain`}
+      style={{ filter: "brightness(0) saturate(100%) invert(12%) sepia(40%) saturate(2000%) hue-rotate(200deg) brightness(90%) contrast(95%)" }}
       {...props}
     />
   );
